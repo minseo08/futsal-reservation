a6c9f0c9cd1cda65a241a3d5348960e26d5b7dcd
김민서 <kimminseo@gimminseoui-MacBookAir.local>
2026-01-30 14:44:21 +0900

    fix: fieldclient page update

--- frontend/app/fields/[id]/page.tsx
+++ frontend/app/fields/[id]/page.tsx
@@ -1,7 +1,17 @@
 import FieldDetailClient from './FieldDetailClient';
 
-export function generateStaticParams() {
-  return [{ id: '1' }];
+export async function generateStaticParams() {
+  try {
+    const res = await fetch('http://futsal-backend-alb-2038761267.ap-northeast-2.elb.amazonaws.com/fields');
+    const fields = await res.json();
+
+    return fields.map((field: any) => ({
+      id: field.id,
+    }));
+  } catch (error) {
+    console.error("ID 목록을 가져오지 못했습니다. 기본값 사용:", error);
+    return [];
+  }
 }
 
 export default async function FieldPage(props: { params: Promise<{ id: string }> }) {
